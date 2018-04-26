87c11362db7c6ea46e571ee003c73879bbd3a165
medlinj <medlinj@rose-hulman.edu>
2018-04-26 11:23:49 -0400

    testing

--- sandbox_motors_new/person2_motors.py
+++ sandbox_motors_new/person2_motors.py
@@ -1,18 +1,21 @@
 """
 Functions for SPINNING the robot LEFT and RIGHT.
-Authors: David Fisher, David Mutchler and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+Authors: David Fisher, David Mutchler and JD Medlin.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
-# TODO: 2. Implment spin_left_seconds, then the relevant part of the test function.
+# TODO: 2. Implement spin_left_seconds, then the relevant part of the test function.
 #          Test and correct as needed.
 #   Then repeat for spin_left_by_time.
 #   Then repeat for spin_left_by_encoders.
 #   Then repeat for the spin_right functions.
 
 
+
 import ev3dev.ev3 as ev3
 import time
 
+left_motor = ev3.LargeMotor(ev3.OUTPUT_B)
+right_motor = ev3.LargeMotor(ev3.OUTPUT_C)
 
 def test_spin_left_spin_right():
     """
@@ -28,6 +31,20 @@ def test_spin_left_spin_right():
       3. Same as #2, but runs spin_left_by_encoders.
       4. Same as #1, 2, 3, but tests the spin_right functions.
     """
+    r1 = input('How long should the duration of movement be?')
+    while True:
+        if r1 == 0:
+            break
+    r2 = input('How fast should motors move from -100 to 100?')
+    r3 = input('How should motors stop? (brake, coast, hold)')
+
+    left_motor.run_forever(speed_sp=r2*8)
+    right_motor.run_forever(speed_sp=r2*8)
+
+    time.sleep(r1)
+
+    left_motor.stop(stop_action=r3)
+    right_motor.stop(stop_action=r3)
 
 
 def spin_left_seconds(seconds, speed, stop_action):
@@ -36,6 +53,14 @@ def spin_left_seconds(seconds, speed, stop_action):
     where speed is between -100 (full speed spin_right) and 100 (full speed spin_left).
     Uses the given stop_action.
     """
+
+    left_motor.run_forever(speed_sp=speed)
+    right_motor.run_forever(speed_sp=-speed)
+
+    time.sleep(seconds)
+
+    left_motor.stop(stop_action=stop_action)
+    right_motor.stop(stop_action=stop_action)
 
 
 def spin_left_by_time(degrees, speed, stop_action):
@@ -48,6 +73,14 @@ def spin_left_by_time(degrees, speed, stop_action):
       2. Sleep for the computed number of seconds.
       3. Stop moving.
     """
+    time = degrees/speed
+    left_motor.speed_sp(speed_sp=speed)
+    right_motor.speed_sp(speed_sp=-speed)
+
+    time.sleep(time)
+
+    left_motor.stop(stop_action=stop_action)
+    right_motor.stop(stop_action=stop_action)
 
 
 def spin_left_by_encoders(degrees, speed, stop_action):
