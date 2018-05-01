ef5dc32f41c7da31496d48634addac7b62f0c63f
Todd Kuebelbeck <kuebeltm@rose-hulman.edu>
2018-05-01 15:10:56 -0400

    Commit

--- sandbox_motors_new/person1_motors.py
+++ sandbox_motors_new/person1_motors.py
@@ -1,7 +1,7 @@
 """
 Functions for moving the robot FORWARD and BACKWARD.
-Authors: David Fisher, David Mutchler and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+Authors: David Fisher, David Mutchler and Todd Kuebelbeck.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 # TODO: 2. Implment forward_seconds, then the relevant part of the test function.
 #          Test and correct as needed.
@@ -36,6 +36,23 @@ def forward_seconds(seconds, speed, stop_action):
     Uses the given stop_action.
     """
 
+    ev3.Sound.speak("I'm sorry Bill, I'm afraid I can't do that").wait()
+
+    # Connect two large motors on output ports B and C
+    left_motor = ev3.LargeMotor(ev3.OUTPUT_B)
+    right_motor = ev3.LargeMotor(ev3.OUTPUT_C)
+
+    # Check that the motors are actually connected
+    assert left_motor.connected
+    assert right_motor.connected
+
+    left_motor.run_forever(speed_sp=speed)
+    right_motor.run_forever(speed_sp=speed)
+    time.sleep(seconds)
+    left_motor.stop(stop_action=stop_action)
+    right_motor.stop(stop_action=stop_action)
+
+    ev3.Sound.speak("Daft world")
 
 def forward_by_time(inches, speed, stop_action):
     """
@@ -47,6 +64,11 @@ def forward_by_time(inches, speed, stop_action):
       2. Sleep for the computed number of seconds.
       3. Stop moving.
     """
+    true_speed = 8*speed
+    sec = (800/(speed*inches))
+
+    forward_seconds(sec,true_speed, stop_action)
+    ev3.Sound.speak("I am the ma ma ma machine").wait()
 
 
 def forward_by_encoders(inches, speed, stop_action):
@@ -57,18 +79,29 @@ def forward_by_encoders(inches, speed, stop_action):
       1. Compute the number of degrees the wheels should spin to achieve the desired distance.
       2. Move until the computed number of degrees is reached.
     """
+    degrees = (360*inches)/4.08
+
+    left_motor = ev3.LargeMotor(ev3.OUTPUT_B)
+    right_motor = ev3.LargeMotor(ev3.OUTPUT_C)
+
+    # Check that the motors are actually connected
+    assert left_motor.connected
+    assert right_motor.connected
+
+    left_motor.run_to_abs_pos(position_sp = degrees, speed_sp = speed, stop_action = stop_action)
 
 
 def backward_seconds(seconds, speed, stop_action):
     """ Calls forward_seconds with negative speeds to achieve backward motion. """
+    forward_seconds(seconds, speed*-1, stop_action = stop_action)
 
 
 def backward_by_time(inches, speed, stop_action):
     """ Calls forward_by_time with negative speeds to achieve backward motion. """
-
+    forward_by_time(inches, speed*-1, stop_action)
 
 def backward_by_encoders(inches, speed, stop_action):
     """ Calls forward_by_encoders with negative speeds to achieve backward motion. """
-
+    forward_by_encoders(inches, speed*-1, stop_action)
 
 test_forward_backward()
