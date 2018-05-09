b9c2cf3baafb4c5a714ebeba4fb61729bcd727e3
medlinj <medlinj@rose-hulman.edu>
2018-05-09 11:38:22 -0400

    Done

--- libs/robot_controller.py
+++ libs/robot_controller.py
@@ -49,13 +49,13 @@ class Snatch3r(object):
 
     def turn_left(self,degrees, speed, stop_action='brake'):
         right_motor = ev3.LargeMotor(ev3.OUTPUT_C)
-        right_motor.run_to_rel_pos(speed_sp=speed * 8, position_sp=degrees * 4.2)
+        right_motor.run_to_rel_pos(speed_sp=speed * 8, position_sp=degrees * 13)
         right_motor.wait_while(ev3.LargeMotor.STATE_RUNNING)
         right_motor.stop(stop_action=stop_action)
 
     def turn_right(self, degrees, speed, stop_action='brake'):
         left_motor = ev3.LargeMotor(ev3.OUTPUT_B)
-        left_motor.run_to_rel_pos(speed_sp=speed * -8, position_sp=degrees * -4.2)
+        left_motor.run_to_rel_pos(speed_sp=speed * -8, position_sp=degrees * -13)
         left_motor.wait_while(ev3.LargeMotor.STATE_RUNNING)
         left_motor.stop(stop_action=stop_action)
 
