a77d6938ab24d87ba6aa4cc918af4d11e07b8f6c
medlinj <medlinj@rose-hulman.edu>
2018-05-15 20:01:09 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	libs/robot_controller.py

--- sandbox/JDMedlin/mqtt/m5_pc_remote_drive.py
+++ sandbox/JDMedlin/mqtt/m5_pc_remote_drive.py
@@ -30,11 +30,11 @@ from tkinter import ttk
 import mqtt_remote_method_calls as com
 
 
-
 def main():
     # DONE: 2. Setup an mqtt_client.  Notice that since you don't need to receive any messages you do NOT need to have
     # a MyDelegate class.  Simply construct the MqttClient with no parameter in the constructor (easy).
     mqtt_client = com.MqttClient()
+    mqtt_client.connect_to_ev3()
 
     root = tkinter.Tk()
     root.title("MQTT Remote")
