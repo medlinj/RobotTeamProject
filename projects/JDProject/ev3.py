5b1945fddb3930932114f2984bcb51c2cdcf01c3
medlinj <medlinj@rose-hulman.edu>
2018-05-16 14:12:16 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	libs/robot_controller.py

--- projects/JDProject/ev3.py
+++ projects/JDProject/ev3.py
@@ -9,6 +9,10 @@ class RobotDelegate(object):
         self.left_paw = ev3.LargeMotor(ev3.OUTPUT_B)
         self.right_paw = ev3.LargeMotor(ev3.OUTPUT_C)
         self.head = ev3.MediumMotor(ev3.OUTPUT_A)
+        self.touch_sensor = ev3.TouchSensor()
+        self.color_sensor = ev3.ColorSensor()
+        self.lights = ev3.Leds
+
 
     def bark(self):
         ev3.Sound.speak('woof, woof. bark, bark').wait()
@@ -17,10 +21,25 @@ class RobotDelegate(object):
         self.left_paw.run_forever(speed_sp=left_paw_speed)
         self.right_paw.run_forever(speed_sp=right_paw_speed)
 
-    def shake_hand(self):
-        self.head.run_to_rel_pos()
+    def head_up(self):
+        self.head.run_forever(speed_sp=700)
+        while not self.touch_sensor.is_pressed:
+            time.sleep(0.01)
+        self.head.stop(stop_action='brake')
+        ev3.Sound.beep().wait()
+
+    def head_down(self):
+        self.head.run_to_rel_pos(position_sp=-14.2*180)
+        self.head.wait_while(ev3.Motor.STATE_RUNNING)
+
+    def shake(self):
+        self.head_up()
+        self.head_down()
         for k in range(5):
-            self.head.run_to_rel_pos()
+            self.head.run_to_rel_pos(position_sp=360, speed_sp=800)
+            self.head.wait_while(ev3.Motor.STATE_RUNNING)
+            self.head.run_to_rel_pos(position_sp=-360, speed_sp=800)
+            self.head.wait_while(ev3.Motor.STATE_RUNNING)
 
     def wag_tail(self):
         for k in range(10):
@@ -28,40 +47,41 @@ class RobotDelegate(object):
                 self.right_paw.run_forever(speed_sp=-600)
                 self.left_paw.run_forever(speed_sp=600)
                 time.sleep(0.75)
+                self.right_paw.stop(stop_action='brake')
+                self.left_paw.stop(stop_action='brake')
             else:
                 self.right_paw.run_forever(speed_sp=600)
                 self.left_paw.run_forever(speed_sp=-600)
                 time.sleep(0.75)
+                self.right_paw.stop(stop_action='brake')
+                self.left_paw.stop(stop_action='brake')
 
     def stay(self):
         self.left_paw.stop(stop_action='brake')
         self.right_paw.stop(stop_action='brake')
         self.head.stop(stop_action='brake')
 
+    def nap_time(self):
+        self.head.stop()
+        self.left_paw.stop()
+        self.right_paw.stop()
+        self.running = False
+
+    def sniff(self):
+        self.color_sensor
+
+    def pet_parade(self):
+        self.lights.RIGHT()
+        self.lights.set_color(self.lights.LEFT, list)
+        
 
 
 
-    # def arm_up(self):
-    #     self.arm.run_forever(speed_sp=700)
-    #     while not self.touch_sensor.is_pressed:
-    #         time.sleep(0.01)
-    #     self.arm.stop(stop_action='brake')
-    #     ev3.Sound.beep().wait()
-    #
-    # def arm_down(self):
-    #     self.arm.run_to_rel_pos(position_sp=-14.2*360)
-    #     self.arm.wait_while(ev3.Motor.STATE_RUNNING)
-    #
     # def loop_forever(self):
     #     self.running = True
     #     while self.running:
     #         time.sleep(0.1)
     #
-    # def shutdown(self):
-    #     self.arm.stop()
-    #     self.left_motor.stop()
-    #     self.right_motor.stop()
-    #     self.running = False
     #
     # def quit_program(mqtt_client, shutdown_ev3):
     #     if shutdown_ev3:
--- projects/JDProject/pc.py
+++ projects/JDProject/pc.py
@@ -25,6 +25,7 @@ def main():
     right_speed_entry.insert(0, "600")
     right_speed_entry.grid(row=1, column=2)
 
+    # Buttons below
 
     forward_button = ttk.Button(main_frame, text="Walk")
     forward_button.grid(row=2, column=1)
@@ -37,8 +38,29 @@ def main():
     stop_button['command'] = lambda: stay(mqtt_client)
     root.bind('<space>', lambda event: stay(mqtt_client))
 
+    wag_button = ttk.Button(main_frame, text='Wag Tail!')
+    wag_button.grid(row=4, column=0)
+    wag_button['command'] = lambda: wag_tail(mqtt_client)
+    root.bind('<e>', lambda event: wag_tail(mqtt_client))
+
+    shake_button = ttk.Button(main_frame, text='Shake Hand!')
+    shake_button.grid(row=5, column=0)
+    shake_button['command'] = lambda: shake(mqtt_client)
+    root.bind('<q>', lambda event: shake(mqtt_client))
+
+    stay_button = ttk.Button(main_frame, text='Stay')
+    stay_button.grid(row=6, column=0)
+    stay_button['command'] = lambda: stay(mqtt_client)
+    root.bind('<r>', lambda event: stay(mqtt_client))
+
+    quit_button = ttk.Button(main_frame, text='Nap (Quit)')
+    quit_button.grid(row=6, column= 2)
+    quit_button['command'] = lambda: nap_time(mqtt_client)
+    root.bind('<p>', lambda event: nap_time(mqtt_client))
+
     root.mainloop()
 
+#     Functions below
 
 def send_walk(mqtt_client, left_speed_entry, right_speed_entry):
     l = int(left_speed_entry.get())
@@ -49,6 +71,26 @@ def send_walk(mqtt_client, left_speed_entry, right_speed_entry):
 def stay(mqtt_client):
     print('stay!')
     mqtt_client.send_message('stay')
+
+def wag_tail(mqtt_client):
+    print('happy doggy!!!')
+    mqtt_client.send_message('wag_tail')
+
+def shake(mqtt_client):
+    print('shake')
+    mqtt_client.send_message('shake')
+
+def bark(mqtt_client):
+    print('woof woof bark bark')
+    mqtt_client.send_message('bark')
+
+def nap_time(mqtt_client):
+    print('nap time!')
+    mqtt_client.send_message('nap_time')
+    mqtt_client.close()
+    exit()
+
+
 
 main()
 
